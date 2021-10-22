7e8a5994cc4589bb98f1e859d591922cd4226148
mohit-k-s <mohit9205990@gmail.com>
2021-10-22 15:19:37 +0530

    PlantStatus and PlantModel added

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -2,7 +2,7 @@ import 'dart:convert';
 
 import 'package:flutter/material.dart';
 import 'package:http/http.dart';
-import 'templates/weather_info.dart';
+import 'models/weather_info.dart';
 import './weather_info.dart';
 import 'constants.dart';
 
--- lib/weather_info.dart
+++ lib/weather_info.dart
@@ -3,7 +3,7 @@ import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart';
 
-import './templates/weather_info.dart';
+import 'models/weather_info.dart';
 import './constants.dart';
 
 Future<WeatherInfo> getWeather(String city) async {
