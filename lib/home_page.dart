cfb997d380442c6827c2c1fd3f0b50a89675d5ce
mr_mechman <hrshit13@gmail.com>
2021-10-25 01:00:15 +0530

    commit 8 added plant_data file

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -24,8 +24,91 @@ Future<WeatherInfo> getWeather(String city) async {
 }
 
 
+class Home extends StatefulWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey[200],
+      appBar: AppBar(
+        backgroundColor: Colors.amber[200],
+        title: Text('Main Menu'),
+        centerTitle: true,
+        actions: [
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: IconButton(
+              icon: Icon(Icons.settings),
+              onPressed: (){},
+            ),
+          ),
+        ],
+      ),
+      body: ListView(
+
+        children: [
+           Option_cards(route: "crops",title: 'Crops',link: "https://images.pexels.com/photos/185402/vegetables-fresh-tomatoes-fresh-vegetables-185402.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",),
+          Option_cards(link: "https://images.pexels.com/photos/5245865/pexels-photo-5245865.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940", title: "Weather", route: "weather"),
+          Option_cards(link: "https://images.pexels.com/photos/2749165/pexels-photo-2749165.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940", title: 'Greenhouse account', route: "user_info"),
+         // Option_cards(link: link, title: title, route: route)
+        ],
+      ),
 
 
+
+    );
+  }
+}
+
+class Option_cards extends StatelessWidget {
+String link;
+String title;
+String route;
+Option_cards({required this.link, required this.title,  this.route = ""});
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      margin: EdgeInsets.fromLTRB(35.0, 8.0, 35.0, 8.0),
+       clipBehavior: Clip.antiAlias,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(20.0),),
+        child:Stack(
+          alignment: Alignment.bottomLeft,
+          children:[
+            Ink.image(image: NetworkImage(link),
+              fit: BoxFit.cover,
+              height: 200,
+              child: InkWell(
+                onTap: (){
+                  if(route.isNotEmpty)
+                 { Navigator.pushNamed(context, '/$route'); }
+                },
+              ) ,
+
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Text(title , style: TextStyle(fontWeight: FontWeight.bold, fontSize: 40.0, color: Colors.white),),
+            ),
+
+          ]
+
+
+
+        )
+
+      );
+  }
+}
+
+/*
 class Home extends StatefulWidget {
 
 
@@ -106,3 +189,4 @@ class Abcd extends StatelessWidget {
     );
   }
 }
+*/
