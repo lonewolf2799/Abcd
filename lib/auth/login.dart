d976def32111ac36409aa252f5ac89a543ee8e4c
mohit-k-s <mohit9205990@gmail.com>
2022-05-14 19:25:47 +0530

    fin

--- lib/auth/login.dart
+++ lib/auth/login.dart
@@ -1,6 +1,6 @@
 import 'dart:convert';
 
-import 'package:flutter/cupertino.dart';
+//import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import '../utils/constants.dart';
 import 'package:http/http.dart' as http;
--- lib/templates/user_info_page.dart
+++ lib/templates/user_info_page.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-import 'package:flutter/widgets.dart';
+//import 'package:flutter/widgets.dart';
 
 // ignore: must_be_immutable
 class UserInfo extends StatelessWidget {
